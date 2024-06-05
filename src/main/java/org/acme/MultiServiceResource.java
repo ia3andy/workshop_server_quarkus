f50d6fa038ea33e3375940b0ffef84815bb56f25
Andy Damevin <ia3andy@gmail.com>
2024-06-05 11:40:38 +0100

    Quarkus version

---
+++ src/main/java/org/acme/MultiServiceResource.java
@@ -0,0 +1,33 @@
+package org.acme;
+
+
+import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+@Path(value = "/v1/VisitedService")
+@RunOnVirtualThread
+public class MultiServiceResource {
+
+    @POST
+    @Path("/1")
+    public void service1(String body) throws InterruptedException {
+        Thread.sleep(200);
+    }
+
+    @POST
+    @Path("/2")
+    public void service2(String body) throws InterruptedException {
+
+        Thread.sleep(300);
+    }
+
+    @POST
+    @Path("/3")
+    public void service3(String body) {
+    }
+
+
+
+
+}
---
+++ src/main/java/org/acme/TestForWorkshopResource.java
@@ -0,0 +1,56 @@
+package org.acme;
+
+
+import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+
+import java.util.Random;
+
+@Path(value = "/v1/crawl/")
+@RunOnVirtualThread
+public class TestForWorkshopResource {
+
+    @Path("{a}/{b}")
+    @GET
+    public String randomPage(@PathParam("a") String a, @PathParam("b") String b){
+        return generateHtmlPageWithUrls(100, "crawl/");
+    }
+
+    @Path("/delay/{a}/{b}")
+    @GET
+    public String randomPageWithDelay(@PathParam("a") String a, @PathParam("b") String b){
+        // show what impact the timer has on the threads
+        Random r = new Random();
+        try {
+            Thread.sleep(r.nextInt(10, 200));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return generateHtmlPageWithUrls(100, "crawl/delay/");
+    }
+
+
+    private String generateHtmlPageWithUrls(int numberOfUrls, String url){
+        StringBuilder s = new StringBuilder();
+
+        Random r = new Random();
+
+        s.append("<html>");
+        s.append("<body>");
+
+        for (int i = 0; i < numberOfUrls; i++) {
+            int n = r.nextInt(0,numberOfUrls + 1000);
+            int n1 = r.nextInt(0,numberOfUrls + 1000);
+
+            s.append("<a href=\"http://localhost:8080/v1/").append(url).append(n1).append("/").append(n).append("\">i am a random url ").append(i).append(" ").append(n1).append("</a><br/>");
+        }
+
+        s.append("</body>");
+        s.append("</html>");
+
+        return s.toString();
+    }
+}
